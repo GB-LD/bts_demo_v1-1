47b66cca43278b1c2c4964ffb9816f6862d16883
GB-LD <gbld@mailo.com>
2022-12-12 11:37:45 +0100

    loginForm creation + loginPage integration

---
+++ src/Form/LoginType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse email :',
+                'attr' => [
+                    'placeholder' => "marine.martin@gmail.com"
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe :',
+                'attr' => [
+                    'placeholder' => "mot de passe"
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
